d069d30647053c1c9c0ff3149c5db0eacb15f4e1
Dariusz Okoński <darek200180@gmail.com>
2024-04-15 21:22:42 +0200

    add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,37 @@
+import { useState } from 'react';
+import data from './data';
+import List from './List';
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthday today</h3>
+
+        <List people={people} />
+
+        {people.length > 0 && (
+          <button
+            type='button'
+            className='btn btn-block'
+            onClick={() => setPeople([])}
+          >
+            clear all
+          </button>
+        )}
+        {people.length === 0 && (
+          <button
+            type='button'
+            className='btn btn-block'
+            onClick={() => setPeople(data)}
+          >
+            Get people
+          </button>
+        )}
+      </section>
+    </main>
+  );
 };
 export default App;
